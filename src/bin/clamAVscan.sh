21a4df153209b6fcc24d83bc5703aaff3376a427
Yoel Mayer <Yoel.Mayer@exlibrisgroup.com>
2019-10-22 16:12:12 +0300

    updated plugin

--- src/bin/clamAVscan.sh
+++ src/bin/clamAVscan.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CLAMSCAN=clamscan
+CLAMSCAN=clamdscan
 SCANOPTS="--stdout --no-summary"
 
 STATUS=255
@@ -11,15 +11,15 @@ TARGET=$1
 
 EchoAndExit()
 {
-	echo STATUS=${STATUS}
-	echo CONTENT=${CONTENT}
-	echo AGENT=${AGENT}
-	exit ${STATUS}
+        echo STATUS=${STATUS}
+        echo CONTENT=${CONTENT}
+        echo AGENT=${AGENT}
+        exit ${STATUS}
 }
 
 # Check is file exist
 
-if [ ! -f ${TARGET} ]
+if [ ! -f "${TARGET}" ]
 then
    CONTENT="File not found"
    EchoAndExit
@@ -28,21 +28,21 @@ fi
 
 # Check file size is less than 2GB
 
-file_size=`ls -Ll ${TARGET} | awk '{print $5}'`
-file_size_tb=`echo "$file_size / 1024 / 1024 / 2048" | bc`
+file_size=`ls -Ll "${TARGET}" | awk '{print $5}'`
+file_size_tb=`echo "$file_size / 1024 / 1024 / 4096" | bc`
 
 # by setting the status to 2 (>1) the new plugin will set the scanning result to 'undetermined'
 
 if [ $file_size_tb -ne 0 ]
 then
     STATUS=2
-	CONTENT="Virus Check is limited to a 2GB file size and thus has been ignored"
-	AGENT="REG_SA_DPS"
+        CONTENT="Virus Check is limited to a 4GB file size and thus has been ignored"
+        AGENT="REG_SA_VC_CLAMAV"
     EchoAndExit
 fi
 
 # Scan file
 
-CONTENT=`${CLAMSCAN} ${SCANOPTS} ${TARGET}`
+CONTENT=`${CLAMSCAN} ${SCANOPTS} "${TARGET}"`
 STATUS=$?
 EchoAndExit
